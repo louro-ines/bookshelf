3078b477838c7555731a2e70699bfa8c2c9f6875
Ines Louro <inespazlouro@gmail.com>
2022-01-05 12:55:26 +0000

    added exception handling for destroy! not working in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  #exception handling
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :not_destroyed
+
+  private
+
+  def not_destroyed(error)
+    render json: { errors: error.record.errors }, status: :unprocessable_entity
+  end
 end
