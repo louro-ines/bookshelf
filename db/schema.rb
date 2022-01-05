e64a3acf4a562643e3ac99f501244dcf9749c0ac
Ines Louro <inespazlouro@gmail.com>
2022-01-05 16:25:55 +0000

    added a author model | fixed db

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_120624) do
+ActiveRecord::Schema.define(version: 2022_01_05_162452) do
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "author"
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "age"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "author_id", null: false
+    t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  add_foreign_key "books", "authors"
 end
