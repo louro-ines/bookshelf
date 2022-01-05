21b34148f1aacf8b0b6fec0a58d0857d9aeb285a
Ines Louro <inespazlouro@gmail.com>
2022-01-05 13:40:38 +0000

    created create and delete tests

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -1,16 +1,52 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
-  it 'returns all books' do
-    FactoryBot.create(:book, title:'1984', author:'George Orwell')
-    FactoryBot.create(:book, title:'Animal Farm ', author:'George Orwell')
+  describe 'GET /books' do
+    before do
+      FactoryBot.create(:book, title:'1984', author:'George Orwell')
+      FactoryBot.create(:book, title:'Animal Farm ', author:'George Orwell')
+    end
 
-    get '/api/v1/books'
+    it 'returns all books' do
+      get '/api/v1/books'
 
-    #checks that the response status is a 200
-    expect(response).to have_http_status(:success)
+      #checks that the response status is a 200
+      expect(response).to have_http_status(:success)
 
-    #checks that there are actuall books being returned
-    expect(JSON.parse(response.body).size).to eq(2)
+      #checks that there are actuall books being returned
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
+  end
+
+  describe 'POST /books' do
+    it 'create a new book' do
+
+      # checks that the book was actually created
+      # by comparing the number of rows in the database before and after the post request
+      expect {
+        post '/api/v1/books', params: { book: { title: 'The Bitcoin Standard', author: 'Saifedean Ammous' } }
+      }.to change { Book.count }.from(0).to(1)
+
+      #checks the response code
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe 'DELETE /books/:id' do
+    # creating the factory w/ no lazy loading (!) so that we have a book when the test runs
+    # and assign it to the let variable book
+    let!(:book) { FactoryBot.create(:book, title:'1984', author:'George Orwell') }
+
+    it'deletes a book' do
+
+      # checks that the book was actually deleted
+      # by comparing the number of rows in the database before and after the delete request
+      expect {
+        delete "/api/v1/books/#{book.id}"
+      }.to change { Book.count }.from(1).to(0)
+
+      #checks the response code
+      expect(response).to have_http_status(:no_content)
+    end
   end
 end
