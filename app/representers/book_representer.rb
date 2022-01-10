8201435e9e7666e95c12016b234e2ebe822dfd09
Ines Louro <inespazlouro@gmail.com>
2022-01-10 10:42:16 +0000

    added book representer

---
+++ app/representers/book_representer.rb
@@ -0,0 +1,19 @@
+class BookRepresenter
+  def initialize(book)
+    @book = book
+  end
+
+  def as_json
+      {
+        id: book.id,
+        title: book.title,
+        author_name: "#{book.author.first_name} #{book.author.last_name}",
+        author_age: book.author.age
+      }
+    end
+  end
+
+  private
+
+  attr_reader :book
+end
